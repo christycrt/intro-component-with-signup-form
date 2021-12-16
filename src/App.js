5e7830a68d427c6192a7aa791205637525889a7b
christycrt <christycrt@gmail.com>
2021-12-16 22:07:00 +0700

    [update] show form error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 import "./App.css";
 
+import { useForm } from "react-hook-form";
+
+const InputList = [
+  {
+    name: "firstname",
+    rule: {
+      required: "First name can not be empty",
+    },
+    placeholder: "First Name",
+  },
+  {
+    name: "lastname",
+    rule: {
+      required: "Last name can not be empty",
+    },
+    placeholder: "Last Name",
+  },
+  {
+    name: "email",
+    rule: {
+      required: "Email can not be empty",
+      pattern: {
+        value: /\S+@\S+\.\S+/,
+        message: "Looks like this is not an email",
+      },
+    },
+    placeholder: "Email",
+  },
+  {
+    name: "password",
+    rule: {
+      required: "Password can not be empty",
+    },
+    placeholder: "Password",
+  },
+];
+
 const App = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="App">
       <main>
@@ -23,15 +67,31 @@ const App = () => {
             </p>
           </div>
           <div className="form-box">
-            <form className="free-trial-form">
-              <input type="text" placeholder="First Name" />
-              <input type="text" placeholder="Last Name" />
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
+            <form className="free-trial-form" onSubmit={handleSubmit(onSubmit)}>
+              {InputList.map((item, key) => {
+                return (
+                  <div className="input-form" key={key}>
+                    <input
+                      {...register(item.name, { ...item.rule })}
+                      placeholder={item.placeholder}
+                    />
+                    {errors[item.name] && (
+                      <>
+                        <p className="error">{errors[item.name].message}</p>
+                        <img
+                          className="error-icon"
+                          src="/images/icon-error.svg"
+                          alt="error-icon"
+                        />
+                      </>
+                    )}
+                  </div>
+                );
+              })}
+              <button type="submit" className="claim-btn">
+                CLAIM YOUR FREE TRIAL
+              </button>
             </form>
-            <button type="submit" className="claim-btn">
-              CLAIM YOUR FREE TRIAL
-            </button>
             <p className="term">
               By clicking the button, you are agreeing to our
               <a href="/"> Terms and Services</a>
